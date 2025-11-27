90b77ee99ff2ecc32e997cbe7ebfbc62fec2ba81
Weegs710 <weegs@tutamail.com>
2025-11-27 01:42:16 -0500

    maint: disable unused AAGL game launchers
    - disabled anime-game-launcher
    - disabled honkers-launcher
    - disabled wavey-launcher
    - disabled sleepy-launcher

--- modules/gaming/aagl.nix
+++ modules/gaming/aagl.nix
@@ -4,15 +4,16 @@
   inputs,
   ...
 }:
-with lib; {
+with lib;
+{
   config = mkIf config.mySystem.features.gaming {
     # Anime Game Launchers (Genshin, Honkai, Wuthering Waves, ZZZ)
     programs = {
-      anime-game-launcher.enable = true;      # Genshin Impact
+      anime-game-launcher.enable = false; # Genshin Impact
       honkers-railway-launcher.enable = true; # Honkai: Star Rail
-      honkers-launcher.enable = true;         # Honkai Impact 3rd
-      wavey-launcher.enable = true;           # Wuthering Waves
-      sleepy-launcher.enable = true;          # Zenless Zone Zero
+      honkers-launcher.enable = false; # Honkai Impact 3rd
+      wavey-launcher.enable = false; # Wuthering Waves
+      sleepy-launcher.enable = false; # Zenless Zone Zero
     };
 
     # Use the aagl nixConfig for additional settings
